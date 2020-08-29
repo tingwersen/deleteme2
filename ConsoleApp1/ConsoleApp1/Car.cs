7626d171461441d6400a1be4a59e6dbb51fba80a
Thomas Søby Ingwersen <thomas.soby.ingwersen.ext@visma.com>
2020-08-29 09:57:11 +0200

    Commit 1

---
+++ ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+        public void Drive(int a, int b, int c, int d, int e, int f, int g)
+        {
+
+        }
+    }
+}
